8a9a59fa12a39329380b734543e43c351090b3a8
Felix Ojiambo <felixojiamboe@gmail.com>
2024-04-10 00:53:43 -0700

    add filterbofere to validate user

--- src/main/java/com/ecom/recipesharing/config/AppConfig.java
+++ src/main/java/com/ecom/recipesharing/config/AppConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
@@ -22,6 +23,7 @@ public class AppConfig {
         )).authorizeHttpRequests(
 Authorize-> Authorize.requestMatchers("/api/**").authenticated()
         .anyRequest().permitAll())
+                .addFilterBefore(new JwtTokenValidator(), BasicAuthenticationFilter.class)
                 // .csrf(csrf->csrf.disable())
         .csrf(AbstractHttpConfigurer::disable)
         .cors(cors->cors.configurationSource(corsConfigurationSource()))
