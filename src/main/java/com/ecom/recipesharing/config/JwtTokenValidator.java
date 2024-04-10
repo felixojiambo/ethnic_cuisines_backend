ba6143baef388a81bfcda8c0fb702ec94e7247b8
Felix Ojiambo <felixojiamboe@gmail.com>
2024-04-10 01:01:56 -0700

    filter in token validator

--- src/main/java/com/ecom/recipesharing/config/JwtTokenValidator.java
+++ src/main/java/com/ecom/recipesharing/config/JwtTokenValidator.java
@@ -1,5 +1,35 @@
 package com.ecom.recipesharing.config;
 
-public class JwtTokenValidator {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
 
+import javax.crypto.SecretKey;
+import java.io.IOException;
+
+public class JwtTokenValidator extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String jwt=request.getHeader(JwtConstant.JWT_HEADER);
+        if(jwt!=null){
+            jwt=jwt.substring(7);
+            try {
+                SecretKey key=Keys.hmacShaKeyFor(JwtConstant.JWT_SECRET.getBytes());
+                Claims claims=Jwts.parseBuilder().setSigningKey(key).build().parseClaimsJws(jwt).getBody();
+                String email=String.valueOf(claims.get("email"));
+                Authentication authentication=new UsernamePasswordAuthenticationToken(email,null,null);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }catch (Exception e){
+                throw  new BadCredentialsException("Invalid Token...");
+            }
+        }
+        filterChain.doFilter(request,response);
+    }
 }
