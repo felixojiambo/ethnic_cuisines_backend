9d0794d3399d48ffd0deeea4e687e15231cfe0f2
Felix Ojiambo <felixojiamboe@gmail.com>
2024-04-09 22:29:21 -0700

    defined recipe methods in interface

--- src/main/java/com/ecom/recipesharing/services/RecipeService.java
+++ src/main/java/com/ecom/recipesharing/services/RecipeService.java
@@ -1,4 +1,17 @@
 package com.ecom.recipesharing.services;
 
+import com.ecom.recipesharing.models.Recipe;
+import com.ecom.recipesharing.models.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public interface RecipeService {
+    public Recipe createRecipe(Recipe recipe, User user);
+    public Recipe findRecipeById(Long id) throws  Exception;
+    public  void deleteRecipe(Long id)throws  Exception;
+    public  Recipe updateRecipe(Recipe recipe,Long id)throws Exception;
+    public List<Recipe> findAllRecipe();
+    public  Recipe likeRecipe(Long recipe,User user) throws Exception;
 }
