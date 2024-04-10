2eeb306b489092fe97f5b7e4326d4d491a510e11
Felix Ojiambo <felixojiamboe@gmail.com>
2024-04-10 00:49:03 -0700

    jwt get email from token

---
+++ src/main/java/com/ecom/recipesharing/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.ecom.recipesharing.config;
+
+public class JwtConstant {
+    public static final  String JWT_SECRET="asdfjgfkdxnrwjsdfrkwosdkjfsdgxcvbresadfeskdfj";
+    public static final String JWT_HEADER="Authorization";
+}
