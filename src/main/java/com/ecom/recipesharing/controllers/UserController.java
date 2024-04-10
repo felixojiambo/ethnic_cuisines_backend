eef5a09b2e4319151441d39d56fb43cc20f5b7c2
Felix Ojiambo <felixojiamboe@gmail.com>
2024-04-10 01:56:10 -0700

    get user profile

--- src/main/java/com/ecom/recipesharing/controllers/UserController.java
+++ src/main/java/com/ecom/recipesharing/controllers/UserController.java
@@ -1,41 +1,16 @@
 package com.ecom.recipesharing.controllers;
-
 import com.ecom.recipesharing.models.User;
-import com.ecom.recipesharing.repositories.UserRepository;
+import com.ecom.recipesharing.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
 @RestController
 @RequestMapping("/api")
 public class UserController {
-    @Autowired
-    private UserRepository userRepository;
-
-    @PostMapping("/create-user")
-    public  User createUser(@RequestBody User user) throws Exception {
-        User isExist = userRepository.findByEmail(user.getEmail());
-        if (isExist != null) {
-            throw new Exception("User not found with email " + user.getEmail());
-
-        }return userRepository.save(user);
-    }
-        @DeleteMapping("/delete-user/{userId}")
-        public String deleteUser (@PathVariable Long userId) throws Exception {
-            userRepository.deleteById(userId);
-            return "User deleted successfully";
-        }
-    @GetMapping("/get-user")
-    public List<User> getAllUses () throws Exception {
-        return userRepository.findAll();
-    }
-    }
-//    public User findByEmail(String email) throws Exception {
-//        User user=userRepository.findByEmail(email);
-//        if(user==null){
-//throw new Exception("User not found with email "+email);
-//        }
-//        return user;
-//    }
+   @Autowired
+    private UserService userService;
+   @GetMapping("/profile")
+   private  User findUserByJwt(@RequestHeader("Authorization") String jwt) throws Exception {
+       return userService.findUserByJwt(jwt);
+   }
+}
 
