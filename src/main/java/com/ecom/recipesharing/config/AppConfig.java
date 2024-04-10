48db5232ef4657139d0a4d5e3e0258d693f138bc
Felix Ojiambo <felixojiamboe@gmail.com>
2024-04-10 00:13:03 -0700

    configured allowed origins

--- src/main/java/com/ecom/recipesharing/config/AppConfig.java
+++ src/main/java/com/ecom/recipesharing/config/AppConfig.java
@@ -1,17 +1,49 @@
 package com.ecom.recipesharing.config;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collection;
+import java.util.Collections;
 
 @Configuration
 public class AppConfig {
+    @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception{
         http.sessionManagement(management-> management.sessionCreationPolicy(
                 SessionCreationPolicy.STATELESS
         )).authorizeHttpRequests(
+Authorize-> Authorize.requestMatchers("/api/**").authenticated()
+        .anyRequest().permitAll())
+                // .csrf(csrf->csrf.disable())
+        .csrf(AbstractHttpConfigurer::disable)
+        .cors(cors->cors.configurationSource(corsConfigurationSource()))
+        .httpBasic(Customizer.withDefaults())
+        .formLogin(Customizer.withDefaults());
 
-        )
-        return  null;
+        return  http.build();
+    }
+
+    private CorsConfigurationSource corsConfigurationSource() {
+
+        return  new CorsConfigurationSource() {
+            @Override
+            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                CorsConfiguration cfg=new CorsConfiguration() ;
+                cfg.setAllowedOrigins(Collections.singletonList("*"));
+                cfg.setAllowedMethods(Collections.singletonList("*"));
+                cfg.setAllowedHeaders(Collections.singletonList("*"));
+                cfg.setExposedHeaders(Collections.singletonList("*"));
+                cfg.setMaxAge(3600L);
+                return null;
+            }
+        };
     }
 }
--- src/main/java/com/ecom/recipesharing/controllers/UserController.java
+++ src/main/java/com/ecom/recipesharing/controllers/UserController.java
@@ -8,12 +8,12 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/api")
 public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    @PostMapping("/create")
+    @PostMapping("/create-user")
     public  User createUser(@RequestBody User user) throws Exception {
         User isExist = userRepository.findByEmail(user.getEmail());
         if (isExist != null) {
@@ -21,12 +21,12 @@ public class UserController {
 
         }return userRepository.save(user);
     }
-        @DeleteMapping("/delete/{userId}")
+        @DeleteMapping("/delete-user/{userId}")
         public String deleteUser (@PathVariable Long userId) throws Exception {
             userRepository.deleteById(userId);
             return "User deleted successfully";
         }
-    @GetMapping("/get")
+    @GetMapping("/get-user")
     public List<User> getAllUses () throws Exception {
         return userRepository.findAll();
     }
